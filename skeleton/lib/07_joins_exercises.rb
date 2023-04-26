02e0b9a824f7e0bf873fae6e5ad2e9cc4ad16a62
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-25 17:33:20 -0700

    got to joins excercise

--- skeleton/lib/07_joins_exercises.rb
+++ skeleton/lib/07_joins_exercises.rb
@@ -40,6 +40,20 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+SELECT
+  title
+FROM
+  movies
+JOIN
+  castings
+ON
+  movies.id = castings.movie_id
+JOIN
+  actors
+ON
+  castings.actor_id = actors.id
+WHERE
+  name = 'Harrison Ford';
   SQL
 end
 
@@ -48,6 +62,7 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    
   SQL
 end
 
